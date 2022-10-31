68b9ac22b329afa007d918b3854fe6ed6e956a8e
Simon9877 <46328994+Simon9877@users.noreply.github.com>
2022-10-31 18:57:40 +0100

    removed old project

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.progmini_projekt {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.progmini_projekt to javafx.fxml;
-    exports com.example.progmini_projekt;
-}
